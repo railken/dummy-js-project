d9963274bd933e7ce8f53a0292caa9b6db2a0d1f
railken <railken@protonmail.ch>
2019-01-06 23:31:40 +0100

    testing

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>Book name: {{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+
+import { VeryDeepBook } from '../models/VeryDeepBook'
+
+export default {
+  data () {
+    return {
+      msg: new VeryDeepBook().getName()
+    }
+  }
+}
+</script>
---
+++ src/models/VeryDeepBook.js
@@ -0,0 +1,7 @@
+import { Book } from '@railken/dummy-js-lib'
+
+export class VeryDeepBook extends Book {
+  getName () {
+    return 'Nope'
+  }
+}
